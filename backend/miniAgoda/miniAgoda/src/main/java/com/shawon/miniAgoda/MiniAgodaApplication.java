3cac4474c1ef151b6912b216be5ef52f2b268d63
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-04-01 16:30:31 +0700

    feature: add Spring Boot backend project with initial structure

---
+++ backend/miniAgoda/miniAgoda/src/main/java/com/shawon/miniAgoda/MiniAgodaApplication.java
@@ -0,0 +1,13 @@
+package com.shawon.miniAgoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAgodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAgodaApplication.class, args);
+	}
+
+}
