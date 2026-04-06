9f556581aaff11c5f0e18763f819a17f83510d85
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-04-06 16:54:48 +0700

    Feature: corrected package

--- src/main/java/com/miniagoda/common/config/JwtConfig.java
+++ src/main/java/com/miniagoda/common/config/JwtConfig.java
@@ -1,4 +1,4 @@
-package com.shawon.miniagoda.common.config;
+package com.miniagoda.common.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
