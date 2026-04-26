259749f1701eed7cc34e05d05ea800b3367d246f
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-04-26 15:25:31 +0700

    Create: project is born

---
+++ src/main/java/com/miniagoda/MiniagodaApplication.java
@@ -0,0 +1,13 @@
+package com.miniagoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniagodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniagodaApplication.class, args);
+	}
+
+}
