793c68ccc07dd6f8f5b3344fa11975543f22f128
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-04-30 14:44:54 +0700

    add NoArgsConstructor annotation

--- src/main/java/com/miniagoda/hotel/entity/Hotel.java
+++ src/main/java/com/miniagoda/hotel/entity/Hotel.java
@@ -6,10 +6,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "hotels")
 public class Hotel extends BaseEntity {
