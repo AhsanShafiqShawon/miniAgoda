69f15d3a0353ae67b40d620a17cc92ad5d8118f5
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-01 20:32:18 +0700

    add method for seeding data into room type table in database

--- src/main/java/com/miniagoda/common/seed/DataSeeder.java
+++ src/main/java/com/miniagoda/common/seed/DataSeeder.java
@@ -1,13 +1,16 @@
 package com.miniagoda.common.seed;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.miniagoda.hotel.entity.Hotel;
+import com.miniagoda.hotel.entity.RoomType;
 import com.miniagoda.hotel.repository.HotelRepository;
+import com.miniagoda.hotel.repository.RoomTypeRepository;
 
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
@@ -15,21 +18,56 @@ import tools.jackson.databind.ObjectMapper;
 @Component
 public class DataSeeder implements CommandLineRunner {
     private final HotelRepository hotelRepository;
+    private final RoomTypeRepository roomTypeRepository;
     private final ObjectMapper mapper;
 
-    DataSeeder(HotelRepository hotelRepository, ObjectMapper mapper) {
+    DataSeeder(HotelRepository hotelRepository,
+        RoomTypeRepository roomTypeRepository,
+         ObjectMapper mapper) {
         this.hotelRepository = hotelRepository;
+        this.roomTypeRepository = roomTypeRepository;
         this.mapper = mapper;
     }
     
     @Override
     public void run(String... args) throws Exception {
+        hotelSeed();
+        roomTypeSeed();
+    }
+
+    private void hotelSeed() throws Exception {
         if(hotelRepository.count() > 0) return;
 
         try (InputStream stream = DataSeeder.class.getResourceAsStream("/data/hotels.json")) {
             if (stream == null) throw new RuntimeException("hotels.json not found");
             List<Hotel> hotels = mapper.readValue(stream, new TypeReference<List<Hotel>>() {});
             hotelRepository.saveAll(hotels);
+        }
+    }
+
+    private void roomTypeSeed() throws Exception {
+        if(roomTypeRepository.count() > 0) return;
+
+        try (InputStream stream = DataSeeder.class.getResourceAsStream("/data/room_types.json")) {
+            if (stream == null) throw new RuntimeException("room_types.json not found");
+            List<RoomTypeSeed> seeds = mapper.readValue(stream, new TypeReference<List<RoomTypeSeed>>() {});
+
+            List<RoomType> roomTypes = new ArrayList<>();
+
+            for(RoomTypeSeed seed : seeds) {
+                var hotel = hotelRepository.findByCode(seed.getHotelCode()).
+                orElseThrow(() -> new RuntimeException("Hotel not found " + seed.getHotelCode()));
+
+                RoomType roomType = new RoomType();
+                roomType.setName(seed.getName());
+                roomType.setCapacity(seed.getCapacity());
+                roomType.setPrice(seed.getPrice());
+                roomType.setTotalUnits(seed.getTotalUnits());
+
+                roomType.setHotel(hotel);
+                roomTypes.add(roomType);
+            }
+            roomTypeRepository.saveAll(roomTypes);
         }
     }
 }
