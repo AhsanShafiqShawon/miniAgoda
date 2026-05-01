3398ff581f5473a5fe3d3e212d5bbcc999758c55
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-01 11:16:37 +0700

    add a new method, findByCode

--- src/main/java/com/miniagoda/hotel/repository/HotelRepository.java
+++ src/main/java/com/miniagoda/hotel/repository/HotelRepository.java
@@ -1,6 +1,7 @@
 package com.miniagoda.hotel.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ import com.miniagoda.hotel.entity.Hotel;
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
     List<Hotel> findByCityIgnoreCase(String city);
     List<Hotel> findByNameContainingIgnoreCase(String keyword);
+    Optional<Hotel> findByCode(String code);
 }
