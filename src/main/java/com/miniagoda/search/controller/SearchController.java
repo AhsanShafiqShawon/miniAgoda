550b7f7d13f54933172ed7e7532f95950b916d25
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-10 15:09:16 +0700

    Feature: add SearchController in search feature

---
+++ src/main/java/com/miniagoda/search/controller/SearchController.java
@@ -0,0 +1,31 @@
+package com.miniagoda.search.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miniagoda.search.dto.SearchRequest;
+import com.miniagoda.search.dto.SearchResponse;
+import com.miniagoda.search.service.SearchService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/hotels/search")
+public class SearchController {
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SearchResponse>> search(@Valid @ModelAttribute SearchRequest searchRequest) {
+        List<SearchResponse> hotels = searchService.search(searchRequest);
+        return ResponseEntity.ok(hotels);
+    }
+}
