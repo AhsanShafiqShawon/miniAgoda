bb35a74b6b7b42d3b5b3ea40382946a0ae8d2b38
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-10 22:28:26 +0700

    remove Builder annotation as fake data deleted

--- src/main/java/com/miniagoda/search/dto/SearchResponse.java
+++ src/main/java/com/miniagoda/search/dto/SearchResponse.java
@@ -2,11 +2,9 @@ package com.miniagoda.search.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import lombok.Builder;
 import lombok.Value;
 
 @Value
-@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchResponse {
     String hotelName;
