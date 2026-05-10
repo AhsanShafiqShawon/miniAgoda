6fa9b9dbe4af26594a44b97a782a808c05f2e144
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-10 17:44:47 +0700

    Feature: add RoomType entity of hotel feature

---
+++ src/main/java/com/miniagoda/hotel/entity/RoomType.java
@@ -0,0 +1,41 @@
+package com.miniagoda.hotel.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.miniagoda.common.entity.BaseEntity;
+import com.miniagoda.inventory.entity.Inventory;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "room_type")
+public class RoomType extends BaseEntity {
+    
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer totalRooms;
+    
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+
+    @OneToMany(mappedBy = "roomType")
+    private List<Inventory> inventory;
+}
