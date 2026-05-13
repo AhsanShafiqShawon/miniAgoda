c135c411da54a330484b5fde3d926ea7042ec0ba
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-13 14:20:05 +0700

    add a method findByCityIgnoreCase

--- src/main/java/com/miniagoda/hotel/repository/HotelRepository.java
+++ src/main/java/com/miniagoda/hotel/repository/HotelRepository.java
@@ -1,5 +1,6 @@
 package com.miniagoda.hotel.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -8,5 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.miniagoda.hotel.entity.Hotel;
 
 public interface HotelRepository extends JpaRepository<Hotel, UUID> {
+    List<Hotel> findByCityIgnoreCase(String destination);
     Optional<Hotel> findByCode(String code);
 }
