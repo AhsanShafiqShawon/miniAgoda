7fd51c1309dfd956ceca314cb0c9f3031f6dac97
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-13 14:44:27 +0700

    add roomTypeId int the params

--- src/main/java/com/miniagoda/inventory/service/InventoryService.java
+++ src/main/java/com/miniagoda/inventory/service/InventoryService.java
@@ -1,12 +1,13 @@
 package com.miniagoda.inventory.service;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class InventoryService {
-    public boolean isAvailable(LocalDate checkIn, LocalDate checkOut, Integer guests, Integer rooms) {
+    public boolean isAvailable(UUID roomTypeId, LocalDate checkIn, LocalDate checkOut, Integer guests, Integer rooms) {
         return true;
     }
 }
