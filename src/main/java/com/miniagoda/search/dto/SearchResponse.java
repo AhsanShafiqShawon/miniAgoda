09e2fad9e4cbe5da41824d455145bcedd47d3706
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-13 22:32:53 +0700

    add hotelId as a new field

--- src/main/java/com/miniagoda/search/dto/SearchResponse.java
+++ src/main/java/com/miniagoda/search/dto/SearchResponse.java
@@ -1,6 +1,7 @@
 package com.miniagoda.search.dto;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
@@ -9,6 +10,7 @@ import lombok.Value;
 @Value
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchResponse {
+    UUID hotelId;
     String hotelName;
     String hotelAddress;
     BigDecimal rating;
