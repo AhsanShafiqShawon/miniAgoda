ff94693696e05f1401c30171fa222777829908b5
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-13 15:48:13 +0700

    put params in multiple line for better view

--- src/main/java/com/miniagoda/inventory/repository/InventoryRepository.java
+++ src/main/java/com/miniagoda/inventory/repository/InventoryRepository.java
@@ -1,9 +1,15 @@
 package com.miniagoda.inventory.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.miniagoda.inventory.entity.Inventory;
 
-public interface InventoryRepository extends JpaRepository<Inventory, UUID> {}
+public interface InventoryRepository extends JpaRepository<Inventory, UUID> {
+    List<Inventory> findByRoomTypeAndDateBetween(UUID roomTypeId,
+        LocalDate checkIn,
+        LocalDate checOut);
+}
