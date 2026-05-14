d6c837e6b41f04f67a0fb176225d3f96228dcc0d
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-14 13:07:18 +0700

    Feature: add controller of hotel feature

---
+++ src/main/java/com/miniagoda/hotel/controller/HotelController.java
@@ -0,0 +1,30 @@
+package com.miniagoda.hotel.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miniagoda.hotel.dto.HotelDetailRequest;
+import com.miniagoda.hotel.dto.HotelDetailResponse;
+import com.miniagoda.hotel.service.HotelService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelController {
+
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+    
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<HotelDetailResponse> getHotelDetail(@Valid @ModelAttribute HotelDetailRequest hotelDetailRequest) {
+        HotelDetailResponse detail = hotelService.getHotelDetail(hotelDetailRequest);
+        return ResponseEntity.ok(detail);
+    }
+}
