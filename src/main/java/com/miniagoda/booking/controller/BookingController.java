0fcdd208b66fb925e20b4366337744cda9a440d6
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-14 18:20:58 +0700

    Feature: controller added

---
+++ src/main/java/com/miniagoda/booking/controller/BookingController.java
@@ -0,0 +1,28 @@
+package com.miniagoda.booking.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miniagoda.booking.dto.BookingRequest;
+import com.miniagoda.booking.dto.BookingResponse;
+import com.miniagoda.booking.service.BookingService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/hotels")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    public ResponseEntity<BookingResponse> book(@Valid @ModelAttribute BookingRequest bookingRequest) {
+        BookingResponse response = bookingService.book(bookingRequest);
+        return ResponseEntity.ok(response);
+    }
+}
