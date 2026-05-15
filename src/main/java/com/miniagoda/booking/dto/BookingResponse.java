e9eec682f637075e8e4502d59f4e4de13340cdd2
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-15 15:08:11 +0700

    Feature: implement booking response dto

--- src/main/java/com/miniagoda/booking/dto/BookingResponse.java
+++ src/main/java/com/miniagoda/booking/dto/BookingResponse.java
@@ -1,5 +1,25 @@
 package com.miniagoda.booking.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.miniagoda.booking.entity.BookingStatus;
+
+import lombok.Value;
+
+@Value
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingResponse {
-    
+    private UUID bookingId;
+    private UUID roomTypeId;
+    private String roomTypeName;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private int roomsBooked;
+    private BigDecimal totalPrice;
+    private BookingStatus status;
+    private LocalDateTime expiredAt;
 }
