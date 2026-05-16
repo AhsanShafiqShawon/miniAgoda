ab66b9f66736d0d445b1b59fdb9e6138a7bba704
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-16 14:42:57 +0700

    add a method findByStripePaymentIntentId()

--- src/main/java/com/miniagoda/payment/repository/PaymentRepository.java
+++ src/main/java/com/miniagoda/payment/repository/PaymentRepository.java
@@ -1,9 +1,12 @@
 package com.miniagoda.payment.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.miniagoda.payment.entity.Payment;
 
-public interface PaymentRepository extends JpaRepository<Payment, UUID> {}
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    Optional<Payment> findByStripePaymentIntentId(String stripePaymentIntentId);
+}
