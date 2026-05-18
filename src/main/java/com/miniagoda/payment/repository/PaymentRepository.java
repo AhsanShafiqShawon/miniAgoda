9e214b49b1f346494fd219adeca4bead158b6e99
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-19 03:06:54 +0700

    remove updateStatus()

--- src/main/java/com/miniagoda/payment/repository/PaymentRepository.java
+++ src/main/java/com/miniagoda/payment/repository/PaymentRepository.java
@@ -5,9 +5,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.miniagoda.payment.entity.Payment;
-import com.miniagoda.payment.entity.PaymentStatus;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     Payment findByGatewayPaymentId(String id);
-    void updateStatus(UUID id, PaymentStatus status);
 }
