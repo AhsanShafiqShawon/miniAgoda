153ebe2d9dbd34567349fe94349490d8970d73db
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-19 03:37:02 +0700

    add exceptions

--- src/main/java/com/miniagoda/payment/gateway/PaymentGateway.java
+++ src/main/java/com/miniagoda/payment/gateway/PaymentGateway.java
@@ -7,7 +7,7 @@ import com.miniagoda.payment.dto.RefundResponse;
 
 public interface PaymentGateway {
 
-    PaymentIntentResponse createPayment(PaymentGatewayRequest request);
-    RefundResponse refund(RefundGatewayRequest request);
-    PaymentEvent parseWebhook(String payload, String signature);
+    PaymentIntentResponse createPayment(PaymentGatewayRequest request) throws Exception;
+    RefundResponse refund(RefundGatewayRequest request) throws Exception;
+    PaymentEvent parseWebhook(String payload, String signature) throws Exception;
 }
