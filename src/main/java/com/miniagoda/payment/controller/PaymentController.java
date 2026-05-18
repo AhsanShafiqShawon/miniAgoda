75c37e5d8e78cb151bb4f4c4d5d42815c419c69d
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-19 00:58:29 +0700

    Feature: add controller of payment feature

---
+++ src/main/java/com/miniagoda/payment/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.miniagoda.payment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miniagoda.payment.dto.PaymentIntentRequest;
+import com.miniagoda.payment.dto.PaymentIntentResponse;
+import com.miniagoda.payment.dto.RefundRequest;
+import com.miniagoda.payment.dto.RefundResponse;
+import com.miniagoda.payment.service.PaymentService;
+
+@RestController
+@RequestMapping("")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/payments/create-intent")
+    public ResponseEntity<PaymentIntentResponse> createPayment(@RequestBody PaymentIntentRequest request) {
+        return ResponseEntity.ok(paymentService.createPayment(request));
+    }
+
+    @PostMapping("/payments/refund")
+    public ResponseEntity<RefundResponse> refund(@RequestBody RefundRequest request) {
+        return ResponseEntity.ok(paymentService.refund(request));
+    }
+
+    @PostMapping("/webhooks/stripe")
+    public ResponseEntity<String> handleWebHook(@RequestBody String payload, @RequestHeader("Stripe-Signature") String sigHeader) {
+        paymentService.handleWebHook(payload, sigHeader);
+        return ResponseEntity.ok("OK");
+    }
+}
