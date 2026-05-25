958e9117c53bb30c6a4a11682b22091aa49018aa
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-26 06:29:30 +0700

    add findByToken() and deleteByUser()

--- src/main/java/com/miniagoda/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/miniagoda/auth/repository/RefreshTokenRepository.java
@@ -1,9 +1,14 @@
 package com.miniagoda.auth.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.miniagoda.auth.entity.RefreshToken;
+import com.miniagoda.user.entity.User;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {}
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByToken(String hashedToken);
+    void deleteByUser(User user);
+}
