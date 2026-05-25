713f710b7a79217da8bf61e84f475d5510d492eb
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-25 11:58:22 +0700

    Feature: User entity and Role enum added

---
+++ src/main/java/com/miniagoda/user/entity/Role.java
@@ -0,0 +1,8 @@
+package com.miniagoda.user.entity;
+
+public enum Role {
+    GUEST,
+    CUSTOMER,
+    HOTEL_ADMIN,
+    SUPER_ADMIN
+}
---
+++ src/main/java/com/miniagoda/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.miniagoda.user.entity;
+
+import java.util.List;
+
+import com.miniagoda.booking.entity.Booking;
+import com.miniagoda.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+}
