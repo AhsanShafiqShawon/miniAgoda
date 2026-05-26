00ddf24dc0fac12f3b708f4b0b6eca008a71a0e9
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-26 11:13:14 +0700

    add RedisTemplate bean

--- src/main/java/com/miniagoda/common/config/AppConfig.java
+++ src/main/java/com/miniagoda/common/config/AppConfig.java
@@ -2,6 +2,9 @@ package com.miniagoda.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +14,14 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
     }
 }
