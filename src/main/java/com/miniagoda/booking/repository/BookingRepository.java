81cda8002f306b3d67c32639930272d670301ae1
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-26 20:42:58 +0700

    add findByUserId()

--- src/main/java/com/miniagoda/booking/repository/BookingRepository.java
+++ src/main/java/com/miniagoda/booking/repository/BookingRepository.java
@@ -1,9 +1,12 @@
 package com.miniagoda.booking.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.miniagoda.booking.entity.Booking;
 
-public interface BookingRepository extends JpaRepository<Booking, UUID> {}
+public interface BookingRepository extends JpaRepository<Booking, UUID> {
+    List<Booking> findByUserId(UUID userId);
+}
