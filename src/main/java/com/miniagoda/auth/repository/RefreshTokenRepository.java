bafd9c7958eb46a9b05a485235ebbba0ab4237cc
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-26 08:50:06 +0700

    add revokeAllByUser()

--- src/main/java/com/miniagoda/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/miniagoda/auth/repository/RefreshTokenRepository.java
@@ -4,11 +4,17 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.miniagoda.auth.entity.RefreshToken;
 import com.miniagoda.user.entity.User;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     Optional<RefreshToken> findByToken(String hashedToken);
-    void deleteByUser(User user);
+    
+    @Modifying
+    @Query("UPDATE RefreshToken r SET r.revoked = true WHERE r.user = :user")
+    void revokeAllByUser(@Param("user") User user);
 }
