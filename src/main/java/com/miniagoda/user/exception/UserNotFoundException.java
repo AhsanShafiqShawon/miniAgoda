0f0468909a109a6bf8a4949e74f56d4d6f5467a4
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-27 16:01:15 +0700

    add custom exception for user not found

---
+++ src/main/java/com/miniagoda/user/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.miniagoda.user.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super("User not found");
+    }
+}
