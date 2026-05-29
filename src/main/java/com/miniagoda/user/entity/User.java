8a6773e43aee3178f5f898da417c70d48bb28c9c
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-30 03:37:07 +0700

    add field 'verified' to User entity

--- src/main/java/com/miniagoda/user/entity/User.java
+++ src/main/java/com/miniagoda/user/entity/User.java
@@ -40,6 +40,9 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean verified;
+
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Booking> bookings;
 
