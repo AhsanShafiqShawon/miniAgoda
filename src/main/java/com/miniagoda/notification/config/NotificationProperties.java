7c2b348a865b37cbe2e4cf78f70526c6f3c91eb6
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-29 11:58:08 +0700

    rename sendGrid to sendGridProperties

--- src/main/java/com/miniagoda/notification/config/NotificationProperties.java
+++ src/main/java/com/miniagoda/notification/config/NotificationProperties.java
@@ -1,19 +1,26 @@
 package com.miniagoda.notification.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+@Validated
 @ConfigurationProperties(prefix = "notification")
 public class NotificationProperties {
+    
+    @NotBlank(message = "fromEmail is required")
     private String fromEmail;
+
+    @NotBlank(message = "fromName is required")
     private String fromName;
 
-    private SendGrid sendGrid = new SendGrid();
+    private SendGridProperties sendGrid = new SendGridProperties();
 
     @Data
-    public static class SendGrid {
+    public static class SendGridProperties {
         private String apiKey;
     }
 }
