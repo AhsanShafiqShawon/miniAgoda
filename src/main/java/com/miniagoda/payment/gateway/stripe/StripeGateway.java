8099cbcc7192ed55dbea39e6de1db93077735577
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-05-31 18:40:18 +0700

    edit params building process

--- src/main/java/com/miniagoda/payment/gateway/stripe/StripeGateway.java
+++ src/main/java/com/miniagoda/payment/gateway/stripe/StripeGateway.java
@@ -1,5 +1,7 @@
 package com.miniagoda.payment.gateway.stripe;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.miniagoda.payment.config.StripeConfig;
 import com.miniagoda.payment.dto.PaymentGatewayRequest;
 import com.miniagoda.payment.dto.PaymentIntentResponse;
@@ -11,13 +13,11 @@ import com.stripe.exception.SignatureVerificationException;
 import com.stripe.model.Event;
 import com.stripe.model.PaymentIntent;
 import com.stripe.model.Refund;
-import com.stripe.model.StripeObject;
 import com.stripe.net.Webhook;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.stripe.param.RefundCreateParams;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 import org.springframework.util.StringUtils;
 
@@ -38,13 +38,19 @@ public class StripeGateway implements PaymentGateway {
         .setCurrency(request.getCurrency().toLowerCase())
         .putMetadata("bookingId", request.getBookingId().toString())
         .setCaptureMethod(PaymentIntentCreateParams.CaptureMethod.AUTOMATIC)
+        .setAutomaticPaymentMethods(
+            PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
+                .setEnabled(true)
+                .setAllowRedirects(PaymentIntentCreateParams.AutomaticPaymentMethods.AllowRedirects.NEVER)
+                .build()
+        )
         .build();
 
         PaymentIntent intent = PaymentIntent.create(params);
 
         return new PaymentIntentResponse(
             request.getBookingId(),
-            intent.getClientSecret(),
+            intent.getId(),
             request.getAmount(),
             request.getCurrency()
         );
@@ -91,54 +97,47 @@ public class StripeGateway implements PaymentGateway {
             throw new RuntimeException("Stripe: invalid webhook signature", e);
         }
 
-        Optional<StripeObject> stripeObject = event.getDataObjectDeserializer().getObject();
-
-
         return switch (event.getType()) {
-        case "payment_intent.succeeded" -> stripeObject
-                .map(obj -> {
-                    PaymentIntent intent = (PaymentIntent) obj;
-                    return PaymentEvent.builder()
-                            .type(PaymentEvent.Type.PAYMENT_SUCCEEDED)
-                            .paymentId(intent.getId())
-                            .bookingId(intent.getMetadata().get("bookingId"))
-                            .amount(intent.getAmountReceived())
-                            .currency(intent.getCurrency())
-                            .build();
-                }).orElse(unknown());
- 
-        case "payment_intent.payment_failed" -> stripeObject
-                .map(obj -> {
-                    PaymentIntent intent = (PaymentIntent) obj;
-                    String msg = intent.getLastPaymentError() != null
-                            ? intent.getLastPaymentError().getMessage() : "Unknown";
-                    return PaymentEvent.builder()
-                            .type(PaymentEvent.Type.PAYMENT_FAILED)
-                            .paymentId(intent.getId())
-                            .bookingId(intent.getMetadata().get("bookingId"))
-                            .failureMessage(msg)
-                            .build();
-                }).orElse(unknown());
-
-        case "charge.refunded" -> stripeObject
-                .map(obj -> {
-                    com.stripe.model.Charge charge = (com.stripe.model.Charge) obj;
-                    return PaymentEvent.builder()
-                            .type(PaymentEvent.Type.REFUND_SUCCEEDED)
-                            .paymentId(charge.getPaymentIntent())
-                            .amount(charge.getAmountRefunded())
-                            .currency(charge.getCurrency())
-                            .build();
-                }).orElse(unknown());
-
-        default -> {
-            yield unknown();
+            case "payment_intent.succeeded" -> {
+                JsonObject obj = JsonParser.parseString(
+                    event.getDataObjectDeserializer().getRawJson()).getAsJsonObject();
+                yield PaymentEvent.builder()
+                    .type(PaymentEvent.Type.PAYMENT_SUCCEEDED)
+                    .paymentId(obj.get("id").getAsString())
+                    .bookingId(obj.getAsJsonObject("metadata").get("bookingId").getAsString())
+                    .amount(obj.get("amount_received").getAsLong())
+                    .currency(obj.get("currency").getAsString())
+                    .build();
             }
+            case "payment_intent.payment_failed" -> {
+                JsonObject obj = JsonParser.parseString(
+                    event.getDataObjectDeserializer().getRawJson()).getAsJsonObject();
+                JsonObject lastError = obj.has("last_payment_error") && !obj.get("last_payment_error").isJsonNull()
+                    ? obj.getAsJsonObject("last_payment_error") : null;
+                String msg = lastError != null && lastError.has("message")
+                    ? lastError.get("message").getAsString() : "Unknown";
+                yield PaymentEvent.builder()
+                    .type(PaymentEvent.Type.PAYMENT_FAILED)
+                    .paymentId(obj.get("id").getAsString())
+                    .bookingId(obj.getAsJsonObject("metadata").get("bookingId").getAsString())
+                    .failureMessage(msg)
+                    .build();
+            }
+            case "charge.refunded" -> {
+                JsonObject obj = JsonParser.parseString(
+                    event.getDataObjectDeserializer().getRawJson()).getAsJsonObject();
+                yield PaymentEvent.builder()
+                    .type(PaymentEvent.Type.REFUND_SUCCEEDED)
+                    .paymentId(obj.get("payment_intent").getAsString())
+                    .amount(obj.get("amount_refunded").getAsLong())
+                    .currency(obj.get("currency").getAsString())
+                    .build();
+            }
+            default -> unknown();
         };
     }
 
     private PaymentEvent unknown() {
         return PaymentEvent.builder().type(PaymentEvent.Type.UNKNOWN).build();
     }
-
 }
