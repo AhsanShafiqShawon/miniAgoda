d5579cab0defdf9f1f9909abc2d0088089b70e18
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-06-03 18:27:08 +0700

    add separate config file for @EnableJpaAuditing

---
+++ src/main/java/com/miniagoda/common/config/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.miniagoda.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {}
