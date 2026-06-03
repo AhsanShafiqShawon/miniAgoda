0c64e9fafc0931daec9151c4629ea19a3c50f370
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-06-03 22:04:51 +0700

    add @Validated at class level

--- src/main/java/com/miniagoda/booking/controller/BookingController.java
+++ src/main/java/com/miniagoda/booking/controller/BookingController.java
@@ -3,6 +3,7 @@ package com.miniagoda.booking.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
+@Validated
 public class BookingController {
 
     private final BookingService bookingService;
